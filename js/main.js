dbcf8809511419512f2395d674f7ba69ec8ef376
julia <kh_jul@mail.ru>
2022-09-18 13:20:02 +0500

    написала функции получения рандомного числа и проверки длины строки(исправила ошибки)

--- js/main.js
+++ js/main.js
@@ -9,15 +9,14 @@ function getRandomInt(left, right) {
   }
   return Math.floor(Math.random() * (right - left + 1) + left);
 }
-//console.log(getRandomInt(2,1));
 
-function checkMaxLenght(str, maxLenght){
-  if(typeof str !== 'string') {
-    str = String(str);
+function checkMaxLenght(testableStr, maxLenght){
+  if(typeof testableStr !== 'string') {
+    testableStr = String(testableStr);
   }
-  if(str.length <= maxLenght) {
+  if(testableStr.length <= maxLenght) {
     return true;
   }
   return false;
 }
-console.log(checkMaxLenght("qwerty", 6));
+
